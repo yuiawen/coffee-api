203682de81ec83c7372111cac6bd5e35c17ee5f9
Ikhwan Alidae <126540391+yuiawen@users.noreply.github.com>
2025-07-20 13:38:41 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,6 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 // $routes->get('/', 'Home::index');
+$routes->get('/', 'Api\Coffees::index');
 
 $routes->group('api', static function ($routes) {
     
